55ce0b05728081ee678c00610047fb06f4a2ee70
narcissus <1044532074@qq.com>
2022-06-18 20:49:47 +0800

    preparing the ast

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Implement a monkey programming language.
 * The parser we write is a recursive descent parser.
 > There are two main strategies when parsing a programming language: top-down parsing or bottom-up parsing.
 > “Recursive descent parsing” is a variant of top-down parsing.
+* Use Pratt parsing to parsing expressions.
 
-## Related concepts
+## Terminology
 * **lexical analysis, lexing or tokenization:** is the process of converting a sequence of characters into a sequence of lexical tokens.
 * **parser:** A parser is a software component that takes input data (frequently text) and builds a data structure – often some kind of parse tree, abstract syntax tree(AST) or other hierarchical structure – giving a structural representation of the input, checking for correct syntax in the process. The parser is often preceded by a separate lexical analyser, which creates tokens from the sequence of input characters.
--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,18 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 type Node interface {
 	// TokenLiteral returns the literal value of the token it’s associated with.
 	// only for debugging and testing.
 	TokenLiteral() string
+
+	// String this method can print AST nodes for debugging and compare them
+	// with other AST nodes.
+	String() string
 }
 
 type Statement interface {
@@ -25,6 +32,24 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
 // LetStatement three fields:
 // Name to hold the identifier of the binding.
 // Value for the expression that produces the value.
@@ -37,6 +62,22 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
@@ -44,20 +85,43 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
-
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
-	} else {
-		return ""
-	}
-}
+func (i *Identifier) String() string       { return i.Value }
 
 type ReturnStatement struct {
 	Token       token.Token // the 'return' token
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode() {}
-
+func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement fulfills the ast.Statement interface, so we can add it
+// to the Statements slice of ast.Program.
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
