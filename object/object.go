f43edd39ec5ed3ae7dc46ac88bdb9456012c5bab
dengzhicheng.123 <dengzhicheng.123@bytedance.com>
2022-07-01 17:47:19 +0800

    support hash

--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"strings"
 
 	"monkey/ast"
@@ -21,6 +22,7 @@ const (
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
 	ARRAY_OBJ        = "ARRAY"
+	HASH_OBJ         = "HASH"
 )
 
 type Object interface {
@@ -137,6 +139,71 @@ func (ao *Array) Inspect() string {
 	out.WriteString("[")
 	out.WriteString(strings.Join(elements, ", "))
 	out.WriteString("]")
+
+	return out.String()
+}
+
+// Hashable can use this interface in our evaluator to check
+// if the given object is usable as a hash key
+// when we evaluate hash literals or index expressions for hashes.
+type Hashable interface {
+	HashKey() HashKey
+}
+
+// HashKey generate a hash key for an [*object.String, *object.Integer, *object.Boolean]
+// that’s comparable and equal to the hash key of another
+// [*object.String, *object.Integer, *object.Boolean] with the same .Value.
+// But the hash keys for an *object.String must never be equal to
+// the hash key for an *object.Integer or an *object.Boolean.
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
+
+// HashPair hold  the original key object
+// and the value object its mapped to.
+type HashPair struct {
+	Key   Object
+	Value Object
+}
+
+type Hash struct {
+	Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var (
+		out   bytes.Buffer
+		pairs []string
+	)
+
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 
 	return out.String()
 }
